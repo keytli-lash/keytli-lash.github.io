e8a7310cdfe48403a1109c938cfc29f4a78bf6e8
Pedro Augusto Pereira Florentino <pedro@nortorial.com.br>
2026-01-20 17:07:21 -0300

    feat: alterando site.

---
+++ script.js
@@ -0,0 +1,26 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal, .reveal-left, .reveal-right");
+    
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+window.addEventListener("load", reveal);
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
